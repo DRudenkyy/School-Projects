81a249d7669c2ffde1475605b4219854a5c76c27
Jake Brandt <jake.brandt@catertrax.com>
2019-04-29 22:33:05 -0400

    Adding smiley/frowny textures

--- labs/lab6/lab6/c++/Textures.cpp
+++ labs/lab6/lab6/c++/Textures.cpp
@@ -42,22 +42,40 @@ GLuint frowny2_texture;
 ///
 void loadTextures( void )
 {
+    //
+    // Store first texture in texture 0
+
+    glActiveTexture(GL_TEXTURE0);
     smiley2_texture = SOIL_load_OGL_texture(
 		"../images/smiley2.png",
 		SOIL_LOAD_AUTO,
 		SOIL_CREATE_NEW_ID,
 		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Clear out bindings per best practices
+    glBindTexture(GL_TEXTURE_2D, 0);
 	
     if(0 == smiley2_texture)
     {
         printf("SOIL loading error: '%s'\n", SOIL_last_result());
     }
 
+    //
+    // Store second texture in texture 1
+
+    glActiveTexture(GL_TEXTURE1);
     frowny2_texture = SOIL_load_OGL_texture(
 		"../images/frowny2.png",
 		SOIL_LOAD_AUTO,
 		SOIL_CREATE_NEW_ID,
 		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Clear out bindings per best practices
+    glBindTexture(GL_TEXTURE_2D, 0);
 	
     if(0 == frowny2_texture)
     {
@@ -77,14 +95,34 @@ void loadTextures( void )
 ///
 void setUpTextures( GLuint program, int obj )
 {
+    int smiley2TextureLocation;
+    int frowny2TextureLocation;
+
     if (obj == OBJ_QUAD)
     {
-        glBindTexture(GL_TEXTURE_2D, smiley2_texture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // Load our textures into their respective uniforms
 
+        smiley2TextureLocation = glGetUniformLocation(program, "smiley2Texture");
+        frowny2TextureLocation = glGetUniformLocation(program, "frowny2Texture");
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, smiley2_texture);
+        glUniform1i(smiley2TextureLocation, 0);
+
+        glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, frowny2_texture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glUniform1i(frowny2TextureLocation, 1);
+    }
+    else
+    {
+        // Ensure both of our used texture spaces are cleared out
+        // (though technically phong.frag isn't currently accessing
+        // them anyway...)
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 }
